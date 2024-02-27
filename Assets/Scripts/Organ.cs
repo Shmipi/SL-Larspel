27dec8aa06d20e927288b4d7ebb54769e092b61d
Mullvadsmacka <joel@monstermatte.se>
2024-02-27 11:00:53 +0100

    mainMenu

---
+++ Assets/Scripts/Organ.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organ : MonoBehaviour
+{
+
+    [SerializeField] private int organID;
+    [SerializeField] private string organName;
+    [SerializeField] private string organDescription;
+    [SerializeField] private Sprite organSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
